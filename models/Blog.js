5c2ddfe9ab17418e8fb973240fa9a4b2f262c6d5
Hare-Ball <robby.hare@gmail.com>
2021-08-09 22:04:55 -0500

    seeding fixed

---
+++ models/Blog.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes, BelongsTo } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        contents: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+      }
+)
+
+module.exports = Blog;
+  
